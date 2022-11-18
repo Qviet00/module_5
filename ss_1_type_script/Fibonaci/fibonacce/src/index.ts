fd2ef3f82d524f8a4a40dd175d59cc87682ea7fd
unknown <qkietqn@gmail.com>
2022-11-18 16:07:45 +0700

    baitap

--- ss_1_type_script/Fibonaci/fibonacce/src/index.ts
+++ ss_1_type_script/Fibonaci/fibonacce/src/index.ts
@@ -1,2 +1,18 @@
 // happy coding 👻
 console.log("hello world");
+function totalFibonacci(count: number) {
+    let num1 = 0;
+    let num2 = 1;
+    let total;
+    let fibonacci :Array<number> = [];
+    for(let i = 0; i < count; i++){
+        fibonacci.push(num1);
+        total = num1 + num2;
+        num1 = num2;
+        num2 = total;
+    }
+    console.log('fibonacci: '+ fibonacci );
+    console.log('total: ' + total);
+    return total;
+}
+totalFibonacci(15);
